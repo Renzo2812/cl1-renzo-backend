a2b40bd332ab8eb5b520c0d871048db76ee160af
Renzo Mendoza <rmendoza_2812@hotmail.com>
2024-09-22 22:19:18 -0500

    Inicio del proyecto backend

---
+++ src/main/java/pe/edu/cibertec/cl1_renzo_backend/Cl1RenzoBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_renzo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1RenzoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1RenzoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/cl1_renzo_backend/Cl1RenzoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_renzo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1RenzoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
